79ba6d2d42978d52ec6d0a0557314b4c8cc461e7
sijimengya <942164828@qq.com>
2023-09-11 17:10:48 +0800

    555

--- src/main/java/ltd/newbee/mall/NewBeeMallApplication.java
+++ src/main/java/ltd/newbee/mall/NewBeeMallApplication.java
@@ -24,6 +24,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewBeeMallApplication {
     public static void main(String[] args) {
         SpringApplication.run(NewBeeMallApplication.class, args);
-        System.out.println(4);
+        System.out.println(5);
     }
 }
